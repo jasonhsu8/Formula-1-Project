f74bccc3faae9e6ce89a9b3696ed729cf3a205d1
jasonhsu8 <jace.hsu@gmail.com>
2023-12-31 22:34:40 +0000

    testing circleci

--- JJ Notebooks/test_suite.py
+++ JJ Notebooks/test_suite.py
@@ -1,13 +1,23 @@
 import unittest
 import pandas as pd
+import os
 from myFunctions import read_data, dataset_folder_path
 
 class testFunction(unittest.TestCase):
+    dataset_folder_path = 'Dataset/'
+    
+    def setUp(self):
+        # Create the Dataset directory if it doesn't exist
+        os.makedirs(self.dataset_folder_path, exist_ok=True)
+
+    def tearDown(self):
+        # Clean up the Dataset directory after the test
+        os.rmdir(self.dataset_folder_path)
 
     def test_read_data(self):
         # Define a sample CSV file for testing
         sample_data = "sample.csv"
-        sample_content = "column1,column2\nvalue1,value2"
+        sample_content = "column1,column2\nv1,v2"
 
         # Write the sample content to the file
         with open(f"{dataset_folder_path}{sample_data}", "w") as file:
@@ -17,13 +27,12 @@ class testFunction(unittest.TestCase):
         result_df = read_data(sample_data)
 
         # Define the expected DataFrame based on the sample content
-        expected_df = pd.DataFrame({"column1": ["value1"], "column2": ["value2"]})
+        expected_df = pd.DataFrame({"column1": ["v1"], "column2": ["v2"]})
 
         # Compare the actual result with the expected DataFrame
         pd.testing.assert_frame_equal(result_df, expected_df)
 
         # Clean up the sample file after the test
-        import os
         os.remove(f"{dataset_folder_path}{sample_data}")
 
 if __name__ == '__main__':
