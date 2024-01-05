4a6da65c28291e2a1dfee2df5bba01a541724a7e
jasonhsu8 <jace.hsu@gmail.com>
2024-01-05 17:22:40 +0000

    new functions + changed file names + updated visuals

--- myFunctions.py
+++ myFunctions.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Read csv Data fron specific folder function
 dataset_folder_path = 'Dataset/'
@@ -21,3 +23,41 @@ def dnf_count(results_df, race_id_in_results):
             pass
 
     return dnf_counter
+
+# Coefficients for Linear Regression
+# This gets the y intercept and the gradient
+def coef_linear_regression(x,y):
+    n = np.size(x)
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+    ss_xy = np.sum(y*x) - n*y_mean*x_mean
+    ss_xx = np.sum(x*x) - n*x_mean*x_mean
+
+    gradient = ss_xy / ss_xx
+    y_intercept = y_mean - gradient*x_mean
+
+    return(y_intercept, gradient)
+
+
+# Calculates the pearson correlation coeffiecient
+def correlation_coefficient(x,y):
+
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+
+    numerator = np.sum((x - x_mean) * (y - y_mean))
+    x_denominator = np.sum((x - x_mean)**2)
+    y_denominator = np.sum((y - y_mean)**2)
+    denominator = np.sqrt(x_denominator * y_denominator)
+
+    corr_coef = numerator / denominator
+
+    return corr_coef
+
+# Plots the linear regression line on a scatter plot
+def plot_regression_line(x,y,b):
+    plt.scatter(x, y, color='m', marker="o", s=30)
+
+    y_pred = b[0]+b[1]*x
+    plt.plot(x, y_pred, color="g")
+    plt.show()
